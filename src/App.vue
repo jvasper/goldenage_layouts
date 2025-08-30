97a8d024f7767a47e699f9773f90e11b3d71c22d
jvasper <klohistaitv@gmail.com>
2025-08-30 21:26:41 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/Tablet.vue'
+</script>
+
+<template>
+  <div>
+    <img class="tablet_image" src="./assets/tablet.svg" alt="">
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
---
+++ src/components/Tablet.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
